fc0d4111e81a854d93cc8415c80f972343449796
PinkDisorder <peanutmacaroon@outlook.com>
2024-12-19 21:55:29 +0200

    Expand en.json with an array of new strings for use in the mod. Expanded the ICommanderConfig Class to include the kits Expanded IMessages so it can work with the current language file

--- VividVoidsCommander/ICommanderConfig.cs
+++ VividVoidsCommander/ICommanderConfig.cs
@@ -1,7 +1,18 @@
-namespace VividVoidsCommander
-{
-	public class ICommanderConfig {
-		public string IsSelfSettable { get; set; }
-		public string CanRoleSwap { get; set; }
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+
+namespace VividVoidsCommander {
+	public class CommanderConfig {
+		public string Relocatable { get; init; }
+		public string CanRelocate { get; init; }
+		public string CanUseKits { get; init; }
+		public List<Kit> Kits { get; set; } = new();
 	}
+	public class Kit {
+		public string Name { get; set; }
+		public List<JsonItemStack> Items { get; set; } = new List<JsonItemStack>();
+		public int Uses { get; set; }
+	}
+
+
 }
--- VividVoidsCommander/IMessages.cs
+++ VividVoidsCommander/IMessages.cs
@@ -1,9 +1,21 @@
 namespace VividVoidsCommander {
-	public class IMessages {
-		public string MissingArg { get; set; }
-		public string IAmArgumentName { get; set; }
-		public string IAmSuccess { get; set; }
-		public string IAmDescription { get; set; }
-		public string IAmCmdName { get; set; }
+	public class Messages {
+		public string MissingArgument { get; init; }
+
+		public string RelocateCmdName { get; init; }
+		public string RelocateParamName { get; init; }
+		public string RelocateDescription { get; init; }
+		public string RelocateSuccess { get; init; }
+		public string RelocateBadSpawn { get; init; }
+		public string RelocateInvalidOption { get; init; }
+		
+		public string KitCmdName { get; init; }
+		public string KitParamName { get; init; }
+		public string KitDescription { get; init; }
+		public string KitSuccess { get; init; }
+		public string KitNotFound { get; init; }
+		public string KitUsesMaxReached { get; init; }
+		public string KitMalformed { get; init; }
+		public string KitDeletionSuccess { get; init; }
 	}
 }
