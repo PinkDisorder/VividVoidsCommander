d870c031c03bf17591549775c7995a7c84e1b0b1
PinkDisorder <peanutmacaroon@outlook.com>
2024-12-26 07:26:08 +0200

    Refactoring

--- VividVoidsCommander/CommanderConfig.cs
+++ VividVoidsCommander/CommanderConfig.cs
@@ -3,18 +3,20 @@ using Vintagestory.API.Common;
 
 namespace VividVoidsCommander {
 	public class CommanderConfig {
+		
+		public List<Kit> Kits { get; init; } = new();
+		
 		public string Relocatable { get; init; }
 		public string CanRelocate { get; init; }
 		public string CanUseKits { get; init; }
 		public string CanMakeKits { get; init; }
 		public string CanDelKits { get; init; }
-		public List<Kit> Kits { get; init; } = new();
 		public string Path { get; set; }
 	}
-	public class Kit {
-		public string Name { get; init; }
-		public List<JsonItemStack> Items { get; init; } = new();
-		public int Uses { get; init; }
+	public record Kit {
+		public required string Name { get; init; }
+		public required List<JsonItemStack> Items { get; init; } = new();
+		public required int Uses { get; init; }
 	}
 
 
